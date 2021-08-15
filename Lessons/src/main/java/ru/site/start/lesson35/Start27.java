a578021bed899d42e0ea518154504f7c66718fff
Grigoriy <pga.profile@gmail.com>
2021-08-15 13:11:42 +1000

    Update lessons

--- Lessons/src/main/java/ru/site/start/lesson35/Start27.java
+++ Lessons/src/main/java/ru/site/start/lesson35/Start27.java
@@ -6,34 +6,34 @@ public class Start27 {
 // Задание 27
 // Что будет результатом компиляции и запуска данного кода?
 
-class A {
-
-    int a = 3;
-
-    int returnA() {
-        System.out.println("Klass A " + a);
-        return a;
-    }
-
-}
-
-class B extends A {
-
-    int a = 5;
-
-    public int returnA() {
-        System.out.println("Klass B " + a);
-        return a;
-    }
-
-    public static void main(String[] args) {
-        A test1 = new B();
-        System.out.println(test1.a + " " + test1.returnA());
-        B test2 = (B) test1;
-        System.out.println(test2.a + " " + test2.returnA());
-    }
-
-}
+//class A {
+//
+//    int a = 3;
+//
+//    int returnA() {
+//        System.out.println("Klass A " + a);
+//        return a;
+//    }
+//
+//}
+//
+//class B extends A {
+//
+//    int a = 5;
+//
+//    public int returnA() {
+//        System.out.println("Klass B " + a);
+//        return a;
+//    }
+//
+//    public static void main(String[] args) {
+//        A test1 = new B();
+//        System.out.println(test1.a + " " + test1.returnA());
+//        B test2 = (B) test1;
+//        System.out.println(test2.a + " " + test2.returnA());
+//    }
+//
+//}
 
 // Результат вывода:
 
